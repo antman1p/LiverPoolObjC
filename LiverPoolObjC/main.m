80d8f2f6ca6903b3031e916e0bfa71ecef99db1a
Antonio Piazza <antman1p@gmail.com>
2021-12-16 11:38:42 -0600

    Initial Commit

---
+++ LiverPoolObjC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LiverPoolObjC
+//
+//  Created by Johnny Mnemonic on 12/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
